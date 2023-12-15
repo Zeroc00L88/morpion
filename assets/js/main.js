e6489edf898f62ed67c33434ab1d6fc5143141b1
Jerome <jeromelesne@gmail.com>
2023-12-15 17:17:38 +0100

    add check functions, link all documents

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+let array = [
+    [1, 2, 2],
+    [0, 2, 1],
+    [2, 0, 2],
+];
+
+const playerOne = 1;
+const playerTwo = 2;
+
+//check horiizontally if full of 1 or 2 and return the index of the row, else null
+function checkH(array, player) {
+    rowNb = null;
+    array.forEach((e, i) => {
+        if (e.every((v) => v == player)) {
+            row = i;
+        }
+    });
+    return rowNb;
+}
+
+//check vertically if full of 1 or 2 and return the "index" of the column, else null
+function checkV(array, player) {
+    let colNb = null;
+    array.forEach((elmt, i) => {
+        let arrayTemp = [];
+        array.forEach((e) => {
+            arrayTemp.push(e[i]);
+        });
+        if (arrayTemp.every((v) => v == player)) {
+            colNb = i;
+        }
+    });
+    return colNb;
+}
+
+//check in diagonal if full of 1 or 2 and return the diagonal 1 (for a backslah-like) and 2 (for a slash-like)
+function checkD(array, player) {
+    let diagNb = null;
+    let arrayTemp = [];
+    array.forEach((e, i) => {
+        arrayTemp.push(e[i]);
+    });
+    if (arrayTemp.every((v) => v == player)) {
+        diagNb = 1;
+    } else {
+        let i = array.length - 1;
+        arrayTemp = [];
+        array.forEach((e) => {
+            arrayTemp.push(e[i]);
+            i--;
+        });
+        if (arrayTemp.every((v) => v == player)) {
+            diagNb = 2;
+        }
+    }
+    return diagNb;
+}
